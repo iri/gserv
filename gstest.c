be2569e5b9ed2be4a8ef0e2a95fc758de842d165
Ingars Ribners <ram3a12@gmail.com>
2018-09-02 23:59:57 +0300

    Update gstest.c

--- gstest.c
+++ gstest.c
@@ -8,31 +8,38 @@ int main(int argc, char *argv[]) {
    char b[MAX_PACKET_LEN];
    int len;
 
-   len = 0;
-   gs_set_begin(b, 50, &len);
-      gs_scr_clr(b, &len);
-      gs_scr_bgcolor(b, 0x11, 0x11, 0x11, 0xFF, &len);
-      gs_color(b, 0xFF, 0x00, 0x00, 0xFF, &len);
-      gs_point_set(b, 6, &len);
-         gs_point(b, 200, 200, &len);
-         gs_point(b, 201, 200, &len);
-         gs_point(b, 202, 200, &len);
-         gs_point(b, 203, 200, &len);
-         gs_point(b, 300, 200, &len);
-         gs_point(b, 301, 200, &len);
-      gs_color(b, 0x00, 0xFF, 0x00, 0xFF, &len);
-      gs_point_set(b, 4, &len);
-         gs_point(b, 200, 220, &len);
-         gs_point(b, 201, 220, &len);
-         gs_point(b, 202, 220, &len);
-         gs_point(b, 203, 220, &len);
-      gs_color(b, 0xFF, 0xFF, 0xFF, 0xFF, &len);
-      gs_line_set(b, 2, &len);
-         gs_line(b, 200, 220, 400, 400, &len);
-         gs_line(b, 300, 250, 300, 400, &len);
-   gs_set_end(b, &len);
+   // Form some test commands (sent as one UDP packet)
+   len = 0;                                              // Init UDP packet length 
+   gs_set_begin(b, 50, &len);                            // Init a command set
+      gs_scr_clr(b, &len);                               // Clear screen
+      gs_scr_bgcolor(b, 0x11, 0x11, 0x11, 0xFF, &len);   // Set background color
+      gs_color(b, 0xFF, 0x00, 0x00, 0xFF, &len);         // Set drawing color (red)
+      gs_point_set(b, 6, &len);                          // Init point set (6 points)
+         gs_point(b, 200, 200, &len);                    //   Draw point
+         gs_point(b, 201, 200, &len);                    //   Draw point
+         gs_point(b, 202, 200, &len);                    //   Draw point
+         gs_point(b, 203, 200, &len);                    //   Draw point
+         gs_point(b, 300, 200, &len);                    //   Draw point
+         gs_point(b, 301, 200, &len);                    //   Draw point
+      gs_color(b, 0x00, 0xFF, 0x00, 0xFF, &len);         // Set drawing color (green)
+      gs_point_set(b, 4, &len);                          // Init point set (4 points)
+         gs_point(b, 200, 220, &len);                    //   Draw point
+         gs_point(b, 201, 220, &len);                    //   Draw point
+         gs_point(b, 202, 220, &len);                    //   Draw point
+         gs_point(b, 203, 220, &len);                    //   Draw point
+      gs_color(b, 0xFF, 0xFF, 0xFF, 0xFF, &len);         // Set drawing color (white)
+      gs_line_set(b, 2, &len);                           // Init line set (2 lines)
+         gs_line(b, 200, 240, 400, 400, &len);           //   Draw line
+         gs_line(b, 300, 270, 300, 400, &len);           //   Draw line
+      gs_color(b, 0x00, 0x00, 0xFF, 0xFF, &len);         // Set drawing color (blue)
+      gs_line_set(b, 4, &len);                           // Init line set (4 lines)
+         gs_line(b, 400, 100, 400, 140, &len);           //   Draw line
+         gs_line(b, 400, 140, 440, 140, &len);           //   Draw line
+         gs_line(b, 440, 140, 440, 100, &len);           //   Draw line
+         gs_line(b, 440, 100, 400, 100, &len);           //   Draw line
+   gs_set_end(b, &len);                                  // Finish the command set
 
-   gs_set_send(b, len, UDP_ADDR, UDP_PORT);
+   gs_set_send(b, len, UDP_ADDR, UDP_PORT);              // Send the command set (as one UDP packet)
 
    return 0;
 }
